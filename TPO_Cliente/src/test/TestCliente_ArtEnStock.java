32ea8bbd520e0180299ff2fb357abd48bd12d01d
tpomfergarcia <tpomfergarcia@gmail.com>
2018-05-20 02:10:41 -0300

    Actualizacion TestCliente_Clientes + nuevos Tests

---
+++ TPO_Cliente/src/test/TestCliente_ArtEnStock.java
@@ -0,0 +1,66 @@
+package test;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.Iterator;
+
+import delegados.SistemaBD;
+import dto.ArticuloEnStockDTO;
+import dto.ItemOCDTO;
+import dto.OrdenDeCompraDTO;
+import excepciones.ExcepcionComunicacion;
+import excepciones.ExcepcionSistema;
+
+public class TestCliente_ArtEnStock {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			SistemaBD bd = new SistemaBD();
+			
+			// Prueba Obtener Orden De Compra
+			
+			int numOCABuscar = 1;
+			OrdenDeCompraDTO ordenOC = bd.obtenerOrdenDeCompra(numOCABuscar);
+			System.out.println("Se obtuvo la Orden De Compra: " + ordenOC.getNumOC());
+
+			// Prueba Cargar Articulos En Stock
+			
+			Collection<ArticuloEnStockDTO> posiciones = new ArrayList<ArticuloEnStockDTO>();
+			ItemOCDTO auxItemOC;
+			int contador = 10;
+			for (Iterator<ItemOCDTO> i = ordenOC.getItems().iterator(); i.hasNext(); ) {
+				auxItemOC = i.next();
+				ArticuloEnStockDTO artEnStock = new ArticuloEnStockDTO();
+				artEnStock.setCodigoBarras(auxItemOC.getArticulo().getCodigoBarras());
+				artEnStock.setCantidad(auxItemOC.getCantidad());
+				artEnStock.setFechaCompra(Calendar.getInstance().getTime());
+				Calendar cal = Calendar.getInstance();
+				cal.setTime(Calendar.getInstance().getTime());
+				cal.add(Calendar.DAY_OF_YEAR, contador);
+				artEnStock.setFechaVencimiento(cal.getTime());
+				artEnStock.setLote(auxItemOC.getArticulo().getCodigoBarras().substring(3));
+				artEnStock.setPrecioCompra(auxItemOC.getArticulo().getPrecioVta()/2);
+				artEnStock.setProveedor(ordenOC.getProveedor());
+				posiciones = bd.cargarArticuloEnStock(ordenOC.getNumOC(), artEnStock);
+				ArticuloEnStockDTO printAES;
+				for (Iterator<ArticuloEnStockDTO> j = posiciones.iterator(); j.hasNext(); ) {
+					printAES = j.next();
+					System.out.println("Posicion " + printAES.getCodigoUbicacion() + ": " + printAES.getCantidad());
+				}
+				contador++;
+			}	
+			
+			// Prueba Cumplir Orden De Compra
+			
+			String nuevoEstado = bd.cumplirOrdenDeCompra(numOCABuscar);
+			System.out.println("El nuevo estado de la Orden De Compra " + numOCABuscar + " es: " + nuevoEstado);
+			
+		} catch (ExcepcionComunicacion e) {
+			System.out.println(e.getMensaje());
+		} catch (ExcepcionSistema es) {
+			System.out.println(es.getMensaje());
+		}
+	}
+}
---
+++ TPO_Cliente/src/test/TestCliente_Articulos.java
@@ -0,0 +1,101 @@
+package test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+import delegados.SistemaBD;
+import dto.ArticuloDTO;
+import dto.OrdenDeCompraDTO;
+import dto.ItemOCDTO;
+import excepciones.ExcepcionComunicacion;
+import excepciones.ExcepcionSistema;
+
+public class TestCliente_Articulos {
+
+	public static void main(String[] args) {
+		try {
+			SistemaBD bd = new SistemaBD();
+
+			// Prueba Alta Articulo
+
+			Collection<ArticuloDTO> articulos = new ArrayList<ArticuloDTO>();
+			ArticuloDTO articulo = new ArticuloDTO();
+			articulo.setCodigoBarras("AAA111");
+			articulo.setDescripcion("Refresco FANTA");
+			articulo.setPresentacion("PACK");
+			articulo.setTamaño(50);
+			articulo.setUnidad("Latas 500ml");
+			articulo.setPrecioVta(1500);
+			articulo.setCantMaxUbicacion(100);
+			articulo.setCantFijaCompra(1000);
+			articulo = bd.altaArticulo(articulo);
+			System.out.println("Alta Articulo: " + articulo.getCodigoBarras() + ", " + articulo.getDescripcion() + " " + articulo.getPresentacion() + " " + articulo.getTamaño() + " " + articulo.getUnidad());
+			articulos.add(articulo);
+			
+			articulo = new ArticuloDTO();
+			articulo.setCodigoBarras("BBB222");
+			articulo.setDescripcion("Refresco FANTA");
+			articulo.setPresentacion("CAJON");
+			articulo.setTamaño(8);
+			articulo.setUnidad("Botellas 1500ml");
+			articulo.setPrecioVta(320);
+			articulo.setCantMaxUbicacion(50);
+			articulo.setCantFijaCompra(100);
+			articulo = bd.altaArticulo(articulo);
+			System.out.println("Alta Articulo: " + articulo.getCodigoBarras() + ", " + articulo.getDescripcion() + " " + articulo.getPresentacion() + " " + articulo.getTamaño() + " " + articulo.getUnidad());
+			articulos.add(articulo);
+			
+			articulo = new ArticuloDTO();
+			articulo.setCodigoBarras("CCC333");
+			articulo.setDescripcion("Arroz Gallo Oro");
+			articulo.setPresentacion("Pack");
+			articulo.setTamaño(10);
+			articulo.setUnidad("Bolsas 1kg");
+			articulo.setPrecioVta(200);
+			articulo.setCantMaxUbicacion(1000);
+			articulo.setCantFijaCompra(500);
+			articulo = bd.altaArticulo(articulo);
+			System.out.println("Alta Articulo: " + articulo.getCodigoBarras() + ", " + articulo.getDescripcion() + " " + articulo.getPresentacion() + " " + articulo.getTamaño() + " " + articulo.getUnidad());
+			articulos.add(articulo);
+
+			
+			// Prueba Obtener Articulo
+
+			articulo = bd.obtenerArticulo("BBB222");
+			System.out.println("Se obtuvo el articulo: " + articulo.getCodigoBarras() + ", " + articulo.getDescripcion());;
+			
+			
+			// Prueba Obtener Catalogo
+
+			Collection<ArticuloDTO> catalogo = new ArrayList<ArticuloDTO>();
+			catalogo = bd.obtenerCatalogo();
+			System.out.println("Articulos del catalogo");
+			ArticuloDTO auxArt;
+			for (Iterator<ArticuloDTO> i = catalogo.iterator(); i.hasNext(); ) {
+				auxArt = i.next();
+				System.out.println(auxArt.getCodigoBarras() + ", " + auxArt.getDescripcion());
+			}
+			
+			
+			// Prueba Generar Orden De Compra
+			
+			String proveedor = "FEMSA COCA-COLA";
+			OrdenDeCompraDTO ordenOC = bd.generarOrdenDeCompra(proveedor, articulos);
+			System.out.println("El numero de OC es: " + ordenOC.getNumOC());
+			ItemOCDTO auxItemOC;
+			int contador = 1;
+			for (Iterator<ItemOCDTO> j = ordenOC.getItems().iterator(); j.hasNext(); ) {
+				auxItemOC = j.next();
+				System.out.println("Item " + contador + ": " + auxItemOC.getArticulo().getCodigoBarras() + ", cant: " + auxItemOC.getCantidad());
+				contador++;
+			}
+				
+		} catch (ExcepcionComunicacion e) {
+			System.out.println(e.getMensaje());
+		} catch (ExcepcionSistema es) {
+			System.out.println(es.getMensaje());
+		}
+	}
+	
+}
