956f33836581221b26f356b51b8c8a558b8cb9a5
tpomfergarcia <tpomfergarcia@gmail.com>
2018-05-20 20:46:55 -0300

    Agregado de pruebas en TestCliente_Clientes

--- TPO_Cliente/src/test/TestCliente_Clientes.java
+++ TPO_Cliente/src/test/TestCliente_Clientes.java
@@ -12,10 +12,10 @@ public class TestCliente_Clientes {
 		try {
 			SistemaBD bd = new SistemaBD();
 			
-			// Prueba alta Cliente Empresa
-			/*
+			// Prueba Alta Cliente Empresa
+	
 			ClienteEmpresaDTO cteEmpresa = new ClienteEmpresaDTO();
-			cteEmpresa.setCuit("30-29777999-7");
+			cteEmpresa.setCuit("30-22333444-5");
 			cteEmpresa.setRazonSocial("Todo fiestas");
 			DireccionDTO dirFacturacion = new DireccionDTO();
 			dirFacturacion.setCalle("Cabildo");
@@ -28,15 +28,44 @@ public class TestCliente_Clientes {
 			cteEmpresa.setLimiteCredito(90000);
 			cteEmpresa = bd.altaClienteEmpresa(cteEmpresa);
 			System.out.println("Cliente Empresa: " + cteEmpresa.getIdCliente() + " " + cteEmpresa.getRazonSocial());;
-			*/
-		
-			// Prueba alta Cliente Persona
-			/*
+
+			cteEmpresa = new ClienteEmpresaDTO();
+			cteEmpresa.setCuit("33-33444555-6");
+			cteEmpresa.setRazonSocial("Kiosco Josesito");
+			dirFacturacion = new DireccionDTO();
+			dirFacturacion.setCalle("Aranguren");
+			dirFacturacion.setNumero(1028);
+			dirFacturacion.setCodigoPostal("1405");
+			dirFacturacion.setLocalidad("C.A.B.A");
+			cteEmpresa.setDireccionFacturacion(dirFacturacion);
+			cteEmpresa.setTipoFactura('B');
+			cteEmpresa.setCondicionesEspeciales("Venta solo efectivo");
+			cteEmpresa.setLimiteCredito(0);
+			cteEmpresa = bd.altaClienteEmpresa(cteEmpresa);
+			System.out.println("Cliente Empresa: " + cteEmpresa.getIdCliente() + " " + cteEmpresa.getRazonSocial());;
+
+			cteEmpresa = new ClienteEmpresaDTO();
+			cteEmpresa.setCuit("33-44555666-7");
+			cteEmpresa.setRazonSocial("Supermercados Chinos & CIA");
+			dirFacturacion = new DireccionDTO();
+			dirFacturacion.setCalle("Donato Alvarez");
+			dirFacturacion.setNumero(235);
+			dirFacturacion.setCodigoPostal("1406");
+			dirFacturacion.setLocalidad("C.A.B.A");
+			cteEmpresa.setDireccionFacturacion(dirFacturacion);
+			cteEmpresa.setTipoFactura('B');
+			cteEmpresa.setCondicionesEspeciales("Venta con cheque a 30 dias");
+			cteEmpresa.setLimiteCredito(100000);
+			cteEmpresa = bd.altaClienteEmpresa(cteEmpresa);
+			System.out.println("Cliente Empresa: " + cteEmpresa.getIdCliente() + " " + cteEmpresa.getRazonSocial());;
+	
+			// Prueba Alta Cliente Persona
+
 			ClientePersonaDTO ctePersona = new ClientePersonaDTO();
-			ctePersona.setDni("25666888");
+			ctePersona.setDni("25666777");
 			ctePersona.setApellido("San Martin");
 			ctePersona.setNombre("Jose");
-			DireccionDTO dirFacturacion = new DireccionDTO();
+			dirFacturacion = new DireccionDTO();
 			dirFacturacion.setCalle("Cachimayo");
 			dirFacturacion.setNumero(230);
 			dirFacturacion.setCodigoPostal("1406");
@@ -47,23 +76,99 @@ public class TestCliente_Clientes {
 			ctePersona.setLimiteCredito(0);
 			ctePersona = bd.altaClientePersona(ctePersona);
 			System.out.println("Cliente Persona: " + ctePersona.getIdCliente() + " " + ctePersona.getApellido() + ", " + ctePersona.getNombre());
-			*/
-			
+
+			ctePersona = new ClientePersonaDTO();
+			ctePersona.setDni("25777888");
+			ctePersona.setApellido("Guemes");
+			ctePersona.setNombre("Miguel");
+			dirFacturacion = new DireccionDTO();
+			dirFacturacion.setCalle("Melincue");
+			dirFacturacion.setNumero(450);
+			dirFacturacion.setCodigoPostal("1425");
+			dirFacturacion.setLocalidad("C.A.B.A");
+			ctePersona.setDireccionFacturacion(dirFacturacion);
+			ctePersona.setTipoFactura('C');
+			ctePersona.setCondicionesEspeciales("Venta con tarjeta");
+			ctePersona.setLimiteCredito(20000);
+			ctePersona = bd.altaClientePersona(ctePersona);
+			System.out.println("Cliente Persona: " + ctePersona.getIdCliente() + " " + ctePersona.getApellido() + ", " + ctePersona.getNombre());
+
+			ctePersona = new ClientePersonaDTO();
+			ctePersona.setDni("25888999");
+			ctePersona.setApellido("Belgrano");
+			ctePersona.setNombre("Manuel");
+			dirFacturacion = new DireccionDTO();
+			dirFacturacion.setCalle("Neuquen");
+			dirFacturacion.setNumero(390);
+			dirFacturacion.setCodigoPostal("1407");
+			dirFacturacion.setLocalidad("C.A.B.A");
+			ctePersona.setDireccionFacturacion(dirFacturacion);
+			ctePersona.setTipoFactura('C');
+			ctePersona.setCondicionesEspeciales("Venta con tarjeta");
+			ctePersona.setLimiteCredito(50000);
+			ctePersona = bd.altaClientePersona(ctePersona);
+			System.out.println("Cliente Persona: " + ctePersona.getIdCliente() + " " + ctePersona.getApellido() + ", " + ctePersona.getNombre());
+	
 			/* Prueba obtener Cliente*/
 			/* Prueba obtener ClienteEmpresa */
 			/* Prueba obtener ClientePersona */
-			int idClienteAObtener = 6;
-			char tipoCliente = bd.obtenerTipoCliente(idClienteAObtener);
-			System.out.println("El tipo de cliente a buscar es: " + tipoCliente);
-			if (tipoCliente == 'E') {
-				ClienteEmpresaDTO cteEmpresa = bd.obtenerCteEmpresa(idClienteAObtener);
-				System.out.println("Cliente Empresa: " + cteEmpresa.getIdCliente() + " " + cteEmpresa.getRazonSocial());
+			/*
+			int idClienteAObtener1 = 6;
+			char tipoCliente1 = bd.obtenerTipoCliente(idClienteAObtener1);
+			System.out.println("El tipo de cliente a buscar es: " + tipoCliente1);
+			if (tipoCliente1 == 'E') {
+				ClienteEmpresaDTO cteEmpresa2 = bd.obtenerCteEmpresa(idClienteAObtener1);
+				System.out.println("Cliente Empresa: " + cteEmpresa2.getIdCliente() + " " + cteEmpresa2.getRazonSocial());
 			}
 			else {
-				ClientePersonaDTO ctePersona = bd.obtenerCtePersona(idClienteAObtener);
-				System.out.println("Cliente Persona: " + ctePersona.getIdCliente() + " " + ctePersona.getApellido() + ", " + ctePersona.getNombre());
+				ClientePersonaDTO ctePersona2 = bd.obtenerCtePersona(idClienteAObtener1);
+				System.out.println("Cliente Persona: " + ctePersona2.getIdCliente() + " " + ctePersona2.getApellido() + ", " + ctePersona2.getNombre());
 			}
-
+			*/
+			/* Prueba Modificar Cliente Empresa */
+			/* Prueba Modificar Cliente Persona */
+			/*			
+			int idClienteAObtener2 = 2;
+			char tipoCliente2 = bd.obtenerTipoCliente(idClienteAObtener2);
+			System.out.println("El tipo de cliente a buscar es: " + tipoCliente2);
+			if (tipoCliente2 == 'E') {
+				ClienteEmpresaDTO cteEmpresa3 = bd.obtenerCteEmpresa(idClienteAObtener2);
+				System.out.println("Cliente Empresa: " + cteEmpresa3.getIdCliente() + " " + cteEmpresa3.getRazonSocial());
+				cteEmpresa3.setLimiteCredito(200000);
+				cteEmpresa3 = bd.modificarCteEmpresa(cteEmpresa3);
+				System.out.println("Cliente Empresa Modificado: " + cteEmpresa3.getIdCliente() + " " + cteEmpresa3.getLimiteCredito());
+			}
+			else {
+				ClientePersonaDTO ctePersona3 = bd.obtenerCtePersona(idClienteAObtener2);
+				System.out.println("Cliente Persona: " + ctePersona3.getIdCliente() + " " + ctePersona3.getApellido() + ", " + ctePersona3.getNombre());
+				dirFacturacion = new DireccionDTO();
+				dirFacturacion.setCalle("Pasaje El Delta");
+				dirFacturacion.setNumero(300);
+				dirFacturacion.setCodigoPostal("2001");
+				dirFacturacion.setLocalidad("Avellaneda");
+				ctePersona3.setDireccionFacturacion(dirFacturacion);
+				ctePersona3 = bd.modificarCtePersona(ctePersona3);
+				System.out.println("Cliente Persona Modificado: " + ctePersona3.getIdCliente() + " " + ctePersona3.getDireccionFacturacion().getCalle());
+			}
+			*/
+			
+			// Prueba Baja Cliente Empresa 
+			// Prueba Baja Cliente Persona
+			/*
+			int idClienteADarDeBaja = 5;
+			bd.bajaCliente(idClienteADarDeBaja);
+			char tipoCliente3 = bd.obtenerTipoCliente(idClienteADarDeBaja);
+			System.out.println("El tipo de cliente a buscar es: " + tipoCliente3);
+			if (tipoCliente3 == 'E') {
+				ClienteEmpresaDTO cteEmpresa4 = bd.obtenerCteEmpresa(idClienteADarDeBaja);
+				System.out.println("Cliente Empresa dado de baja: " + cteEmpresa4.getIdCliente() + " " + cteEmpresa4.getEstado());
+			}
+			else {
+				ClientePersonaDTO ctePersona4 = bd.obtenerCtePersona(idClienteADarDeBaja);
+				System.out.println("Cliente Persona: " + ctePersona4.getIdCliente() + " " + ctePersona4.getEstado());
+			}
+			*/
+			
 		} catch (ExcepcionComunicacion e) {
 			System.out.println(e.getMensaje());
 		} catch (ExcepcionSistema es) {
